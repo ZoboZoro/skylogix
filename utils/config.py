c8d32f4a0fa14372d8c69697889ba19c686b6e50
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-12-31 13:58:03 +0100

    pipeline config

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='/home/taofeecoh/skylogix/.env', verbose=True)
+
+# MongoDB config
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
+CONNECTION_STRING = f"mongodb://{MONGO_USER}:{MONGO_PASS}@localhost:27017/"
