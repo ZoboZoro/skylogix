14cc2b66747740bb56e024431240a2f8b76e9ce3
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-12-31 13:57:36 +0100

    pipeline scripting

---
+++ utils/ingestion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#####################
+# Author: Taofeecoh
+# Date: 30/12/2025
+#
+#
+#####################
+
+echo "`date +\%Y-\%m-\%d,\%H:\%M:\%S` :: initializing..."
+set -e
+python3 /home/taofeecoh/skylogix/utils/ingestion.py
