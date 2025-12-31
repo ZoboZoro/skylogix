d0170cf8e83f33bf777365764622ab3612117e98
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-12-31 12:50:04 +0100

    pipeline

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -1,10 +1,10 @@
-import requests
-from pymongo import MongoClient
-import pandas as pd
-import os
 import logging
+import os
+from datetime import datetime, timezone
+
+import requests
 from dotenv import load_dotenv
-from datetime import datetime
+from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 
 # configure logging
@@ -20,7 +20,7 @@ load_dotenv(dotenv_path='/home/taofeecoh/skylogix/.env', verbose=True)
 # Define variables
 base_url = 'https://api.weatherbit.io/v2.0/forecast/daily'
 params = {
-    'lat': '6.465422', # Lagos
+    'lat': '6.465422',  # Lagos
     'lon': '3.406448',
     'key': os.getenv("API_KEY")
         }
@@ -35,6 +35,7 @@ CONNECTION_STRING = f"mongodb://{MONGO_USER}:{MONGO_PASS}@localhost:27017/"
 def api_to_mongo(
         url: str,
         querystrings: dict,
+        location: list,
         connection: str,
         database: str,
         collection: str,
@@ -45,43 +46,76 @@ def api_to_mongo(
     Function to extract data from an endpoint and store json format data.
     :param url: API endpoint
     :param querystrings: query parameters
+    :param location: list of cities' lat and long
     :param connection: MongoDB connection string
     :param database: target database
     :param collection: target collection
     :param source: data source name
     :param timeout: request timeout in seconds
     """
-
     try:
-        response = requests.get(url, params=querystrings, timeout=timeout)
-        if response.status_code == 200:
-            response = response.json()
-            document = {
-                "source": source,
-                "ingested_at": datetime.now(),
-                "request": {
-                    "url": url,
-                    "params": querystrings
-                },
-                "api_response": response
-            }
-            logging.info("extraction complete!")
-        else:
-            logging.error(f"Error!: {response.text}")
-    except Exception as e:
-        logging.exception(f"Connection error:, {e}")
-
-    # Initiate connection to staging database
-    try:
+        # Initiate connection to staging database
         with MongoClient(connection) as client:
             db_collection = client[database][collection]
-            db_collection.insert_one(document)
 
-        logging.info(
-            f"Successfully ingested data from"
-            f"{source} into {database}.{collection}"
-        )
+        # Fetch data specific to each region's API
+            for city in location:
+                    query = {
+                        **querystrings,
+                        "lat": city["lat"],
+                        "lon": city["lon"]
+                    }
 
+                    try:
+                        response = requests.get(url, params=query, timeout=timeout)
+                        if response.status_code == 200:
+                            response = response.json()
+                            document = {
+                                "source": source,
+                                "city": city["city"],
+                                "ingested_at": datetime.now(tz=timezone.utc),
+                                "request": {
+                                    "url": url,
+                                    "params": query
+                                },
+                                "api_response": response
+                            }
+
+                            logging.info(f"extraction complete!, now ingesting to database: {database}")
+
+                            observation_time = datetime.fromtimestamp(response["data"][0]["ts"], tz=timezone.utc)
+                            filter_query = {
+                                "source": source,
+                                "city": city["city"],
+                                "observation_time": observation_time
+                            }
+
+                            update_query = {
+                                "$set": document
+                            }
+
+                            db_collection.update_one(
+                                filter_query,
+                                update_query,
+                                upsert=True
+                            )
+
+                            logging.info(
+                            f"Weather data upserted for {city['city']} at {observation_time}"
+                            )
+
+                        else:
+                            logging.error(f"Error!: {response.text}")
+                    except Exception as e:
+                        logging.exception(f"API Connection error:, {e}")
     except PyMongoError as e:
         logging.error(f"MongoDB insertion failed: {e}")
         raise
+
+
+# print(api_to_mongo(url=base_url,
+#                 querystrings=params,
+#                 connection=CONNECTION_STRING,
+#                 database='skylogix',
+#                 collection='weather_staging',
+#                 source="weatherbits.io"))
