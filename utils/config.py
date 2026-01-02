7fab964540679586e154777a09677a1d354ea5d1
Taofeecoh <adesanutaofeecoh@gmail.com>
2026-01-02 10:58:11 +0100

    modified configuration connection string

--- utils/config.py
+++ utils/config.py
@@ -7,4 +7,4 @@ load_dotenv(dotenv_path='/home/taofeecoh/skylogix/.env', verbose=True)
 # MongoDB config
 MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASS = os.getenv("MONGO_PASS")
-CONNECTION_STRING = f"mongodb://{MONGO_USER}:{MONGO_PASS}@localhost:27017/"
+CONNECTION_STRING = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@cluster0.81ebuut.mongodb.net/?appName=Cluster0"
